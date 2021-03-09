f98e578147e0b13bcb8b5ea3577701823d48eaef
Ilya Gulyaev <gulyaev13@gmail.com>
2021-03-09 22:06:47 +0300

    Refactor LoginTest

--- src/test/java/com/ifmo/hatchery/api/LoginTest.java
+++ src/test/java/com/ifmo/hatchery/api/LoginTest.java
@@ -1,25 +1,34 @@
-package com.ifmo.hatchery;
+package com.ifmo.hatchery.api;
 
 import org.junit.jupiter.api.Test;
 import java.net.HttpURLConnection;
+import java.util.Map;
 
+import org.junit.jupiter.api.Assertions;
+
+import static com.ifmo.hatchery.init.DataInit.CUSTOMER_DEFAULT_USERNAME;
+import static com.ifmo.hatchery.init.DataInit.DEFAULT_PASSWORD;
 import static io.restassured.RestAssured.given;
 
-public class ApiTest {
+public class LoginTest {
     private static final String API_URL = "http://localhost:8080";
 
     @Test
     public void loginTest() {
-        given()
+        Map<String, String> cookies = given()
                 .baseUri(API_URL)
                 .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                .formParam("username", "usernameDonor")
-                .formParam("password", "password")
+                .formParam("username", CUSTOMER_DEFAULT_USERNAME)
+                .formParam("password", DEFAULT_PASSWORD)
                 .expect()
                 .statusCode(HttpURLConnection.HTTP_MOVED_TEMP)
                 .header("Location", String.format("%s/home", API_URL))
                 .when()
-                .post("/login");
+                .post("/login")
+                .cookies();
+        Assertions.assertNotNull(cookies);
+        Assertions.assertFalse(cookies.isEmpty());
+        Assertions.assertNotNull(cookies.get("JSESSIONID"));
     }
 
     @Test
