83174d2fa572ea85084a689a998f74ddccc90df9
Ilya Gulyaev <gulyaev13@gmail.com>
2021-03-10 05:22:29 +0300

    Fix types in OrderXService

--- src/main/java/com/ifmo/hatchery/service/OrderXService.java
+++ src/main/java/com/ifmo/hatchery/service/OrderXService.java
@@ -1,9 +1,8 @@
 package com.ifmo.hatchery.service;
 
-import com.ifmo.hatchery.model.auth.User;
+import com.ifmo.hatchery.model.auth.UserX;
 import com.ifmo.hatchery.model.system.OrderX;
 import com.ifmo.hatchery.repository.OrderXRepository;
-import com.ifmo.hatchery.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +14,7 @@ public class OrderXService {
     private OrderXRepository<OrderX, Long> orderRepository;
 
 
-    public List<OrderX> findByCustomer(User customer){
+    public List<OrderX> findByCustomer(UserX customer){
         List<OrderX> orderX = orderRepository.findByCustomer(customer);
         return orderX;
     }
