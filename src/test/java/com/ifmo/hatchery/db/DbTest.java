eab5ea6718e0816770f8f86db0d13fcc4010e839
Ilya Gulyaev <gulyaev13@gmail.com>
2021-03-10 17:37:31 +0300

    Add AddDB test

---
+++ src/test/java/com/ifmo/hatchery/db/DbTest.java
@@ -0,0 +1,47 @@
+package com.ifmo.hatchery.db;
+
+import com.ifmo.hatchery.model.auth.UserX;
+import com.ifmo.hatchery.model.system.BioState;
+import com.ifmo.hatchery.model.system.Biomaterial;
+import com.ifmo.hatchery.model.system.BiomaterialType;
+import com.ifmo.hatchery.model.system.Caste;
+import com.ifmo.hatchery.model.system.OrderX;
+import com.ifmo.hatchery.repository.BiomaterialRepository;
+import com.ifmo.hatchery.repository.OrderRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class DbTest {
+    @Autowired
+    private BiomaterialRepository<Biomaterial, Long> biomaterialRepository;
+
+    @Autowired
+    private OrderRepository<OrderX, Long> orderRepository;
+
+    @Test
+    public void biomaterialTest() {
+        Biomaterial biomaterial = new Biomaterial();
+        biomaterial.setBioState(BioState.NOT_USE);
+        biomaterial.setType(BiomaterialType.MALE);
+        biomaterial.setDonor(null);
+        biomaterial = biomaterialRepository.save(biomaterial);
+        assertNotNull(biomaterial.getId());
+    }
+
+    @Test
+    public void orderTest() {
+        OrderX order = new OrderX();
+        order.setCaste(Caste.ALPHA);
+        order.setSkills(Collections.emptyList());
+        order.setCustomer(null);
+
+        order = orderRepository.save(order);
+        assertNotNull(order.getId());
+    }
+}
